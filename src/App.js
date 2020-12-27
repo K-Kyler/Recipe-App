e31c5f4e36d7a9ae4929c7ae3620341ef3fe95c7
K-Kyler <khaiquang690@gmail.com>
2020-12-27 13:23:37 +0700

    Initial app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  // Get app id & key from EDAMAM API
+  const APP_ID = 'ede1a235';
+  const APP_KEY = '8468fd152cd94feeed806e4061f1c3b9';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
