769e9862b4d1869c23053bc935beeab7b317c057
K-Kyler <khaiquang690@gmail.com>
2021-01-12 09:16:32 +0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,85 +1,88 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import Form from './components/Form';
-import Recipe from './components/Recipe';
-import * as ReactBootstrap from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import Recipe from "./components/Recipe";
+import * as ReactBootstrap from "react-bootstrap";
+import { APP_KEY, APP_ID } from "./keys";
 
 function App() {
-  // Get app id & key from EDAMAM API
-  const APP_ID = 'ede1a235';
-  const APP_KEY = '8468fd152cd94feeed806e4061f1c3b9';
+    // Get app id & key from EDAMAM API
 
-  // States
-  const [recipes, setRecipes] = useState([]);
-  const [search, setSearch] = useState("");
-  const [query, setQuery] = useState('coffee');
-  const [searchPlaceholder, setSearchPlaceholder] = useState("Type in your recipes!");
-  const [loading, setLoading] = useState(false);
+    // States
+    const [recipes, setRecipes] = useState([]);
+    const [search, setSearch] = useState("");
+    const [query, setQuery] = useState("coffee");
+    const [searchPlaceholder, setSearchPlaceholder] = useState(
+        "Type in your recipes!"
+    );
+    const [loading, setLoading] = useState(false);
 
-  // Use effect
-  useEffect(() => {
-    getRecipes();
-  }, [query]);
+    // Use effect
+    useEffect(() => {
+        getRecipes();
+    }, [query]);
 
-  // Function to get recipes from EDAMAM API
-  const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
-    const data = await response.json();
+    // Function to get recipes from EDAMAM API
+    const getRecipes = async () => {
+        const response = await fetch(
+            `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+        );
+        const data = await response.json();
 
-    setRecipes(data.hits);
-    console.log(data.hits);
+        setRecipes(data.hits);
+        console.log(data.hits);
 
-    // Check search input and reloading spinner
-    if (data.hits.length == 0) {
-      setSearchPlaceholder("No result!");
-      setSearch("");
-      setLoading(false);
-    }
+        // Check search input and reloading spinner
+        if (data.hits.length == 0) {
+            setSearchPlaceholder("No result!");
+            setSearch("");
+            setLoading(false);
+        } else {
+            setSearchPlaceholder("Type in your recipes!");
+            setLoading(true);
+        }
+    };
 
-    else {
-      setSearchPlaceholder("Type in your recipes!");
-      setLoading(true);
-    }
-  }
+    return (
+        <div className="App">
+            <h1 className="text-center mt-3">
+                Find your favorite recipes here
+            </h1>
 
-  return (
-    <div className="App">
-      <h1 className="text-center mt-3">Find your favorite recipes here</h1>
+            <Form
+                search={search}
+                setSearch={setSearch}
+                setQuery={setQuery}
+                searchPlaceholder={searchPlaceholder}
+                setLoading={setLoading}
+            />
 
-      <Form 
-        search={search} 
-        setSearch={setSearch}
-
-        setQuery={setQuery}
-
-        searchPlaceholder={searchPlaceholder}
-
-        setLoading={setLoading}
-      />
-
-      {loading ? 
-        <div className="container pt-3">
-          <div className="row">
-              {recipes.map((recipe, index) => {
-                return(
-                  <Recipe 
-                    key={index} 
-                    title={recipe.recipe.label}
-                    calories={recipe.recipe.calories}
-                    image={recipe.recipe.image}
-                    ingredients={recipe.recipe.ingredients}
-                  />
-                );
-              })}
-          </div>
-        </div>         
-        : 
-        <div className="text-center mt-5 pt-5">
-          <ReactBootstrap.Spinner animation="border" variant="success" />
+            {loading ? (
+                <div className="container pt-3">
+                    <div className="row">
+                        {recipes.map((recipe, index) => {
+                            return (
+                                <Recipe
+                                    key={index}
+                                    title={recipe.recipe.label}
+                                    calories={recipe.recipe.calories}
+                                    image={recipe.recipe.image}
+                                    ingredients={recipe.recipe.ingredients}
+                                />
+                            );
+                        })}
+                    </div>
+                </div>
+            ) : (
+                <div className="text-center mt-5 pt-5">
+                    <ReactBootstrap.Spinner
+                        animation="border"
+                        variant="success"
+                    />
+                </div>
+            )}
         </div>
-      }
-    </div>
-  );
+    );
 }
 
 export default App;
